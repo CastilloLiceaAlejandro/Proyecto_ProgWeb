33f51582c4bfc9d15fc01533951d37f74fde93d6
CastilloLiceaAlejandro <56612932+CastilloLiceaAlejandro@users.noreply.github.com>
2020-09-27 21:40:20 -0700

    Update models.py

--- models.py
+++ models.py
@@ -2,21 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Producto(models.Model):
-    ID_Juego=models.IntegerField(primary_key=True)
-    Nombre_Juego=models.CharField(max_length=40)
-    Plataforma=models.ForeignKey(Plataforma,on_delete=models.CASCADE)
-    Genero=models.CharField(max_length=10)
-    ESRB=models.CharField(max_length=10)
-    Precio=models.IntegerField()
-    Nombre_Desarrollador=models.ForeignKey(Desarrollador,on_delete=models.CASCADE)
-    Fecha_Lanzamiento=models.DateField()
-    Puntuacion=model.IntegerField()
-    Alta_Producto=models.DateField()
-    Actualizacion_Producto=DateTimeField(auto_now=True)
-    def __str__(self):
-        return self.Nombre_Juego
-
 class Plataforma(models.Model):
     ID_Plataforma=models.IntegerField(primary_key=True)
     Plataforma=models.CharField(max_length=10)
@@ -31,10 +16,24 @@ class Plataforma(models.Model):
 
 class Desarrollador(models.Model):
     ID_Desarrollador=models.IntegerField(primary_key=True)
-    ID_Juego=models.ForeignKey(Producto,on_delete=models.CASCADE)
     Nombre_Desarrollador=models.CharField(max_length=10)
     def __str__(self):
         return self.Nombre_Desarrollador
+
+class Producto(models.Model):
+    ID_Juego=models.IntegerField(primary_key=True)
+    Nombre_Juego=models.CharField(max_length=40)
+    Plataforma=models.ForeignKey(Plataforma,on_delete=models.CASCADE)
+    Genero=models.CharField(max_length=10)
+    ESRB=models.CharField(max_length=10)
+    Precio=models.IntegerField()
+    Nombre_Desarrollador=models.ForeignKey(Desarrollador,on_delete=models.CASCADE)
+    Fecha_Lanzamiento=models.DateField()
+    Puntuacion=models.IntegerField()
+    Alta_Producto=models.DateField()
+    Actualizacion_Producto=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.Nombre_Juego
 
 class Stock(models.Model):
     ID_Juego=models.ForeignKey(Producto,on_delete=models.CASCADE)
