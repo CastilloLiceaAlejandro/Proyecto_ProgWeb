b8d633d00a60824821a55c60b1aa462ea0e4a8b5
CastilloLiceaAlejandro <56612932+CastilloLiceaAlejandro@users.noreply.github.com>
2020-09-27 22:13:22 -0700

    Update models.py

--- models.py
+++ models.py
@@ -39,5 +39,4 @@ class Stock(models.Model):
     ID_Juego=models.ForeignKey(Producto,on_delete=models.CASCADE)
     Cantidad=models.IntegerField()
     def __str__(self):
-        cadena=self.ID_Juego
-        return self.cadena
+        return self.ID_Juego
