20c7fcb608ec0e606e567fb7791d845d2e9d2f64
CastilloLiceaAlejandro <56612932+CastilloLiceaAlejandro@users.noreply.github.com>
2020-10-07 19:49:11 -0700

    Update forms.py

--- App/forms.py
+++ App/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 
 from .models import Producto
+from .models import Plataforma
 
-class FormCreate(forms.ModelForm):
+class FormCreate_Producto(forms.ModelForm):
     class Meta:
         model = Producto
+        fields = "__all__"
+
+class FormCreate_Plataforma(forms.ModelForm):
+    class Meta:
+        model = Plataforma
         fields = "__all__"
