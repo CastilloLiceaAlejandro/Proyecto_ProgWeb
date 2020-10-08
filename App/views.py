7576f12a9cef458dc04e9b9af708569305e8ca0d
CastilloLiceaAlejandro <56612932+CastilloLiceaAlejandro@users.noreply.github.com>
2020-10-07 19:50:52 -0700

    Update views.py

--- App/views.py
+++ App/views.py
@@ -8,32 +8,73 @@ from .models import Plataforma
 from .models import Desarrollador
 from .models import Stock
 
-from .forms import FormCreate
+from .forms import FormCreate_Producto
+from .forms import FormCreate_Plataforma
 
 # Create your views here.
 
 class Lista_Producto(generic.ListView):
-	template_name = "inv/Lista_Producto.html"
+	template_name = "inv/Producto/Lista_Producto.html"
 	model = Producto
 
+class Lista_Plataforma(generic.ListView):
+	template_name = "inv/Plataforma/Lista_Plataforma.html"
+	model = Plataforma
+
+
+									       ### CRUD PRODUCTO ###
+
 class Detail_Producto(generic.DetailView):
-	template_name = "inv/Detail_Producto.html"
+	template_name = "inv/Producto/Detail_Producto.html"
 	model = Producto
 
 class New_Producto(generic.CreateView):
-	template_name = "inv/New_Producto.html"
+	template_name = "inv/Producto/New_Producto.html"
 	model = Producto
-	form_class = FormCreate
-	success_url = reverse_lazy("inv:list")
+	form_class = FormCreate_Producto
+	success_url = reverse_lazy("inv:listproducto")
 
 class Update_Producto(generic.UpdateView):
-	template_name = "inv/Update_Producto.html"
+	template_name = "inv/Producto/Update_Producto.html"
 	model = Producto
-	form_class = FormCreate
-	success_url = reverse_lazy("inv:list")
+	form_class = FormCreate_Producto
+	success_url = reverse_lazy("inv:listproducto")
 
 class Delete_Producto(generic.DeleteView):
-	template_name = "inv/Delete_Producto.html"
+	template_name = "inv/Producto/Delete_Producto.html"
 	model = Producto
-	form_class = FormCreate
-	success_url = reverse_lazy("inv:list")
+	form_class = FormCreate_Producto
+	success_url = reverse_lazy("inv:listproducto")
+
+
+
+									       ### CRUD PLATAFORMA ###
+
+class Detail_Plataforma(generic.DetailView):
+	template_name = "inv/Plataforma/Detail_Plataforma.html"
+	model = Plataforma
+
+class New_Plataforma(generic.CreateView):
+	template_name = "inv/Plataforma/New_Plataforma.html"
+	model = Plataforma
+	form_class = FormCreate_Plataforma
+	success_url = reverse_lazy("inv:listplataforma")
+
+class Update_Plataforma(generic.UpdateView):
+	template_name = "inv/Plataforma/Update_Plataforma.html"
+	model = Plataforma
+	form_class = FormCreate_Plataforma
+	success_url = reverse_lazy("inv:listplataforma")
+
+class Delete_Plataforma(generic.DeleteView):
+	template_name = "inv/Plataforma/Delete_Plataforma.html"
+	model = Plataforma
+	form_class = FormCreate_Plataforma
+	success_url = reverse_lazy("inv:listplataforma")
+
+###############################################3
+def list(request):
+	context= {
+		"Proyecto_producto": Producto.objects.all()
+	}
+	return render(request, "list.html", context)
