5ab975a1358b59c257f09f61199a71866eed1c6e
CastilloLiceaAlejandro <56612932+CastilloLiceaAlejandro@users.noreply.github.com>
2020-10-21 15:22:08 -0700

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .serializers import PlataformaSerializer, DesarrolladorSerializer, ProductoSerializer, StockSerializer
+from .serializers import PlataformaListSerializer, DesarrolladorListSerializer, ProductoListSerializer, StockListSerializer
+from .serializers import PlataformaDetailSerializer, ProductoDetailSerializer
 
 from Proyecto.models import Plataforma, Desarrollador, Producto, Stock
 
@@ -12,8 +13,14 @@ from Proyecto.models import Plataforma, Desarrollador, Producto, Stock
 class PlataformaList(APIView):
     def get(self,request):
         plataforma = Plataforma.objects.all()
-        data = PlataformaSerializer(plataforma , many = True).data
+        data = PlataformaListSerializer(plataforma , many = True).data
         return Response(data)
+class PlataformaDetail(APIView):
+    def get(self,request,pk):
+        plataforma = get_object_or_404(Plataforma, pk = pk)
+        data = PlataformaDetailSerializer(plataforma).data
+        return Response(data)
+
 
 class DesarrolladorList(APIView):
     def get(self,request):
@@ -21,11 +28,18 @@ class DesarrolladorList(APIView):
         data = DesarrolladorSerializer(desarrollador , many = True).data
         return Response(data)
 
+
 class ProductoList(APIView):
     def get(self,request):
         producto = Producto.objects.all()
-        data = ProductoSerializer(producto , many = True).data
+        data = ProductoListSerializer(producto , many = True).data
         return Response(data)
+class ProductoDetail(APIView):
+    def get(self,request,pk):
+        producto = get_object_or_404(Producto, pk = pk)
+        data = ProductoDetailSerializer(producto).data
+        return Response(data)
+
 
 class StockList(APIView):
     def get(self,request):
