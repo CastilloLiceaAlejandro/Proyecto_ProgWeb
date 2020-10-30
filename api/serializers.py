34b34a92c60d795132e781d5ee9a8153c7539f27
CastilloLiceaAlejandro <56612932+CastilloLiceaAlejandro@users.noreply.github.com>
2020-10-29 18:13:47 -0700

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -10,9 +10,21 @@ class PlataformaDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plataforma
         fields = ("ID_Plataforma", "Plataforma", "Empresa")
+class PlataformaNewSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return Plataforma.objects.create(**validated_data)
+    class Meta:
+        model = Plataforma
+        fields = "__all__"
 
 
 class DesarrolladorListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Desarrollador
+        fields = "__all__"
+class DesarrolladorNewSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return Desarrollador.objects.create(**validated_data)
     class Meta:
         model = Desarrollador
         fields = "__all__"
@@ -26,9 +38,21 @@ class ProductoDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
         fields = ("ID_Juego", "Nombre_Juego", "Plataforma", "Precio", "Puntuacion")
+class ProductoNewSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return Producto.objects.create(**validated_data)
+    class Meta:
+        model = Producto
+        fields = "__all__"
 
 
 class StockListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stock
+        fields = "__all__"
+class StockNewSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return Stock.objects.create(**validated_data)
     class Meta:
         model = Stock
         fields = "__all__"
